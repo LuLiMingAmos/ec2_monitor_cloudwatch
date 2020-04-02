1d528f1cdeb24ec0d1aabe092608af4bbcd392d2
liming.lu <liming.lu@LimingLu.local>
2020-04-02 11:52:26 +0800

    init

--- ec2_monitor_cloudwatch.sh
+++ ec2_monitor_cloudwatch.sh
@@ -1,7 +1,7 @@
 if apt-get help 
 then
-	apt-get update;apt-get install wget glibc glibc-dev -y;test -d /opt/src/ec2_monitor || mkdir -p /opt/src/ec2_monitor;cd /opt/src/ec2_monitor;rm -f /opt/src/ec2_monitor/*
-	wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip
+	apt-get update;apt-get install wget glibc glibc-dev -y;test -d /opt/src/ec2_monitor || mkdir -p /opt/src/ec2_monitor;cd /opt/src/ec2_monitor
+	wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip -O AmazonCloudWatchAgent.zip
         cd /opt/src/ec2_monitor;unzip AmazonCloudWatchAgent.zip
 	cat >> /opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json <<EOF
 '{
@@ -38,8 +38,8 @@ EOF
 	/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json -s;/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -a status || systemctl status amazon-cloudwatch-agent
 	
 else
-	yum install -y glibc glibc-devel wget unzip;test -d /opt/src/ec2_monitor || mkdir -p /opt/src/ec2_monitor;cd /opt/src/ec2_monitor;rm -f /opt/src/ec2_monitor/*
-	wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip
+	yum install -y glibc glibc-devel wget unzip;test -d /opt/src/ec2_monitor || mkdir -p /opt/src/ec2_monitor;cd /opt/src/ec2_monitor
+	wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip -O AmazonCloudWatchAgent.zip
         cd /opt/src/ec2_monitor;unzip AmazonCloudWatchAgent.zip
         cat >> /opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json <<EOF
 '{
